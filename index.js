5dea4fff3a66b831bdd70dfb102f3c52d1ab1687
jyoung0930 <jaronyoung1@gmail.com>
2022-09-29 22:17:30 -0500

    Continued Development
    Added more CSS, JavaScript and html

---
+++ index.js
@@ -0,0 +1,13 @@
+// const container = document.querySelector(".quote-container");
+
+// const URL = "https://api.adviceslip.com/advice";
+
+// async function getAdvice() {
+//   const res = await fetch(URL);
+//   const data = await res.json();
+//   const advice = data.slip.advice;
+//   const adviceID = data.slip.id;
+//   container.innerHTML = data.slip.advice;
+// }
+
+// console.log(getAdvice());
